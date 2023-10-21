b1a8af68ad9ac275b7d2b68e2355bea2038e00d0
KlajdiZmalaj <klajdizmalaj@icloud.com>
2023-10-21 22:03:26 +0200

    Next Export for github actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: {
+    // Use the "export" option
+    export: {
+      // Set the value according to your preferences
+      // For example, you can use a specific directory such as 'out'
+      directory: "out",
+    },
+  },
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
 
 module.exports = nextConfig;
